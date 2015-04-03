799f83bb06ee9a01a53ef3d25f35351b7ea29de7
yuusuke0126 <yuusuke.kobayashi.0126@gmail.com>
2015-04-04 02:50:02 +0900

    Modify client.py and server.py

--- client.py
+++ client.py
@@ -8,8 +8,8 @@ Created on Fri Apr  3 23:05:35 2015
 import socket
 import time
 
-host = '127.0.0.1'
-port = 3794
+host = '10.249.255.194'
+port = 9999
 clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientsock.bind((host, port))
 print("wait...")
@@ -18,3 +18,4 @@ time.sleep(1)
 for i in range(10):
     recv_msg, addr = clientsock.recvfrom(1024)
     print("Received ->", int.from_bytes(recv_msg, "big"))
+    time.sleep(1)
