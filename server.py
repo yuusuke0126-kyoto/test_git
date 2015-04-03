29760155c8104ed9bec7262580ea99352756442a
Yusuke Kobayashi <yuusuke.kobayashi.0126@gmail.com>
2015-04-04 02:48:31 +0900

    Modify server.py

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ import socket
 import time
 
 def sendmsg(send_msg):
-    host = '127.0.0.1'
-    port = 3794
+    host = '10.249.255.194'
+    port = 9999
     serversock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print("Type message...")
 #    send_msg = raw_input()
@@ -19,9 +19,9 @@ def sendmsg(send_msg):
 
 if __name__ == "__main__":
     i = 0
-    for i in range(100):
+    for i in range(20):
         j = i +100
         print(j)
         sendmsg(j.to_bytes(1,"big"))
-        time.sleep(0.5)
+        time.sleep(0.05)
     
